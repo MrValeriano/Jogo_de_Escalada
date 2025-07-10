8a304797283b9448b846262832822cbe814067a0
MrValeriano <jpsvalerius@gmail.com>
2025-07-10 11:11:04 +0100

    Reestruturações

--- Grafismos/Mapa/Dados/fundo.tsx
+++ Grafismos/Mapa/Dados/fundo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="fundo" tilewidth="96" tileheight="96" tilecount="9" columns="9">
- <image source="../../Mapa/mapa_tiles.png" width="864" height="96"/>
+ <image source="../mapa_tiles.png" width="864" height="96"/>
 </tileset>
