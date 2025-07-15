8cb46002723ff7ee956060d2bc48dfca7694c919
MrValeriano <jpsvalerius@gmail.com>
2025-07-15 17:10:47 +0100

    Update Personagem_Principal.py

--- Código/Personagem_Principal.py
+++ Código/Personagem_Principal.py
@@ -1,7 +1,8 @@
 from Definições import *
 
 class Principal(pygame.sprite.Sprite):
-    def __init__(self, *groups):
+    def __init__(self, pos, *groups):
         super().__init__(*groups)
         self.image = pygame.image.load("Grafismos/Personagem_Principal/Idle/pixil-frame-0.png")
-        self.rect = self.image.get_rect(center = (600, 300))
+        self.rect = self.image.get_frect(center = pos)
+        self.direction = vector()
