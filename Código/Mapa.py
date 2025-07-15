0d373080ae4d0bc23a0c2447db6042a9e6535756
MrValeriano <jpsvalerius@gmail.com>
2025-07-15 16:46:30 +0100

    Update Mapa.py

--- Código/Mapa.py
+++ Código/Mapa.py
@@ -1,4 +1,5 @@
 from Definições import *
+from Personagem_Principal import *
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, *groups):
@@ -10,21 +11,20 @@ class Mapa:
     def __init__(self):
         self.display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         
-        self.import_assets()
-        self.setup(self.tmx_maps["overworld"], "gamestart")
+        self.importar_dados()
+        self.setup(self.tmx_maps["Mapa"], "Início")
     
-    def import_assets(self):
-        self.tmx_maps = load_pygame(join('Mapa','Dados','overworld.tmx'))
+    def importar_dados(self):
+        self.tmx_maps = {
+            "Mapa": load_pygame(join('Mapa','Dados','Mapa.tmx'))
+        }
     
     def setup(self, tmx_map, player_start_pos):
         #* terrain
-        for layer in ["Terrain", "Terrain Top"]:
+        for layer in ["Fundo", "Paredes"]:
             for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
                 Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
-        #* objects
-        for obj in tmx_map.get_layer_by_name("Objects"):
-            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
         #* entities
-        for obj in tmx_map.get_layer_by_name("Entities"):
-            if obj.name == "Player" and obj.properties["pos"] == player_start_pos:
-                self.player = Player((obj.x, obj.y), self.all_sprites)
+        for obj in tmx_map.get_layer_by_name("Entidades"):
+            if obj.name == "Jogador" and obj.properties["Posição"] == player_start_pos:
+                self.player = Principal((obj.x, obj.y), self.all_sprites)
