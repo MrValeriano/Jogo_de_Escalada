65e81480185210fa5d4824c8350af8d332630f37
MrValeriano <jpsvalerius@gmail.com>
2025-07-15 17:07:14 +0100

    Update Loop_principal_processos_do_jogo.py

--- Código/Loop_principal_processos_do_jogo.py
+++ Código/Loop_principal_processos_do_jogo.py
@@ -7,10 +7,14 @@ jogador = pygame.sprite.GroupSingle()
 jogador.add(Principal())
 
 while True:
+    #* tick
     dt = clock.tick()/1000
+    #* eventos
     event_loop()
+    #* representações no ecrã
+    todos_sprites.update(dt)
     screen.fill('aqua')
-    
+    todos_sprites.draw(screen)
     jogador.draw(screen)
     
     pygame.display.update()
