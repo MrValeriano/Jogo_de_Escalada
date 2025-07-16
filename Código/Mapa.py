126d1778fcf2c33540f41b6c3086d638ee5525fe
MrValeriano <jpsvalerius@gmail.com>
2025-07-16 20:00:21 +0100

    Update Mapa.py

--- Código/Mapa.py
+++ Código/Mapa.py
@@ -51,7 +51,7 @@ class Mapa:
             if obj.name == "Jogador" and obj.properties["Posição"] == pos_inicial_jog:
                 self.posição = (obj.x, obj.y)
             else:
-                Sprite((obj.x, obj.y), obj.image, todos_sprites)
+                self.fim_do_jogo = Sprite((obj.x, obj.y), obj.image, todos_sprites)
         #* fronteiras
         for area in tmx_mapa.get_layer_by_name("Bordas_Colisão"):
             rect = pygame.Rect(area.x, area.y, area.width, area.height)
@@ -67,4 +67,3 @@ class Mapa:
                     coords = (((area.x + area.width) - surf.width), ((area.y + area.height) - surf.height))
                 plataforma = Sprite(coords, surf, todos_sprites)
                 lista_plataformas.append(plataforma)
-        
