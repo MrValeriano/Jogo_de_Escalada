a3322f07bb70dabcb8ebcea7837854110f878f6c
MrValeriano <jpsvalerius@gmail.com>
2025-07-16 19:18:57 +0100

    Update Grafismos

---
+++ Grafismos/Mapa/Dados/plataformas.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="plataformas" tilewidth="576" tileheight="48" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Plataforma_grande.png" width="576" height="48"/>
+ </tile>
+ <tile id="1">
+  <image source="../Plataforma_media.png" width="384" height="48"/>
+ </tile>
+ <tile id="2">
+  <image source="../Plataforma_pequena.png" width="192" height="48"/>
+ </tile>
+</tileset>
