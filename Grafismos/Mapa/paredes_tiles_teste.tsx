9a1d46af7d6172e6769ef9796dac584f65a96014
MrValeriano <jpsvalerius@gmail.com>
2025-07-16 10:42:29 +0100

    Remoção paredes de teste

--- Grafismos/Mapa/paredes_tiles_teste.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="paredes_tiles_teste" tilewidth="96" tileheight="96" tilecount="10" columns="2">
- <image source="paredes_tiles_teste.png" width="192" height="480"/>
-</tileset>
