fd298fd9c49b8cdb3cd12465d5535b4d10717de3
MrValeriano <jpsvalerius@gmail.com>
2025-07-18 13:06:55 +0100

    Update Personagem_Principal.py

--- Código/Personagem_Principal.py
+++ Código/Personagem_Principal.py
@@ -6,13 +6,17 @@ class Principal(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load("Grafismos/Personagem_Principal/Idle/pixil-frame-0.png")
         self.rect = self.image.get_frect(center = pos)
-        self.dire = vector()
-        self.facing = "right"
-        self.frame = 0
+        self.direção = vector()
+        self.lado = "right"
+        self.indice_frame = 0
     
     def movimentação(self, dt):
-        self.rect.center += self.direction * 500 * dt
-    
+        self.rect.center += self.direção * 500 * dt
+
+    def animação(self, dt):
+        self.indice_frame += 4 * dt
+
     def update(self, dt):
-        self.direction = input_jogador()
+        self.direção = input_jogador()
         self.movimentação(dt)
+        self.animação(dt)
