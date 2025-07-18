77233889364b6fa720438bbafd7936b701177f4b
MrValeriano <jpsvalerius@gmail.com>
2025-07-18 19:41:32 +0100

    Update Inimigos.py

--- Código/Inimigos.py
+++ Código/Inimigos.py
@@ -11,14 +11,6 @@ class Principal(pygame.sprite.Sprite):
             "andar": {
                 "direita": importar_pasta("Grafismos","Personagem_Principal","Walk_Cycle","Direita"),
                 "esquerda": importar_pasta("Grafismos","Personagem_Principal","Walk_Cycle","Esquerda")
-            },
-            "salto": {
-                "direita": importar_pasta("Grafismos","Personagem_Principal","Jump_Cycle","Direita"),
-                "esquerda": importar_pasta("Grafismos","Personagem_Principal","Jump_Cycle","Esquerda")
-            },
-            "pendurado": {
-                "direita": importar_pasta("Grafismos","Personagem_Principal","Hang_Cycle","Direita"),
-                "esquerda": importar_pasta("Grafismos","Personagem_Principal","Hang_Cycle","Esquerda")
             }
         }
         self.acção = "parado"
@@ -32,13 +24,11 @@ class Principal(pygame.sprite.Sprite):
         self.rect.center += self.direção * 500 * dt
 
     def animação(self, dt):
-        self.indice_frame += 20 * dt
+        self.indice_frame += ANIMATION_SPEED * dt
         if int(self.indice_frame) >= len(self.frames[self.acção][self.lado]):
             self.indice_frame = 0
-        print(int(self.indice_frame), len(self.frames[self.acção][self.lado]))
         self.image = self.frames[self.acção][self.lado][int(self.indice_frame)]
 
     def update(self, dt):
-        self.direção = input_jogador()
         self.movimentação(dt)
         self.animação(dt)
