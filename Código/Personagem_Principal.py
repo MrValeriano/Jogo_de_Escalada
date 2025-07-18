56e52e124a7d6461c8d02cb05d605d073f98ab06
MrValeriano <jpsvalerius@gmail.com>
2025-07-18 14:02:15 +0100

    Update Personagem_Principal.py

--- Código/Personagem_Principal.py
+++ Código/Personagem_Principal.py
@@ -9,20 +9,32 @@ class Principal(pygame.sprite.Sprite):
                 "direita": importar_pasta("..","Grafismos","Personagem_Principal","Idle","Direita"),
                 "esquerda": importar_pasta("..","Grafismos","Personagem_Principal","Idle","Esquerda")
             },
-            
+            "andar": {
+                "direita": importar_pasta("..","Grafismos","Personagem_Principal","Walk_Cycle","Direita"),
+                "esquerda": importar_pasta("..","Grafismos","Personagem_Principal","Walk_Cycle","Esquerda")
+            },
+            "salto": {
+                "direita": importar_pasta("..","Grafismos","Personagem_Principal","Jump_Cycle","Direita"),
+                "esquerda": importar_pasta("..","Grafismos","Personagem_Principal","Jump_Cycle","Esquerda")
+            },
+            "pendurado": {
+                "direita": importar_pasta("..","Grafismos","Personagem_Principal","Hang_Cycle","Direita"),
+                "esquerda": importar_pasta("..","Grafismos","Personagem_Principal","Hang_Cycle","Esquerda")
+            }
         }
-        self.image = pygame.image.load("Grafismos/Personagem_Principal/Idle/pixil-frame-0.png")
+        self.acção = "parado"
+        self.lado = "direita"
+        self.indice_frame = 0
+        self.image = self.frames[self.acção][self.lado][self.indice_frame]
         self.rect = self.image.get_frect(center = pos)
         self.direção = vector()
-        self.lado = "right"
-        self.indice_frame = 0
     
     def movimentação(self, dt):
         self.rect.center += self.direção * 500 * dt
 
     def animação(self, dt):
         self.indice_frame += 4 * dt
-        self.image = self.frames[int(self.indice_frame % len(self.frames))]
+        self.image = self.frames[self.acção][self.lado][int(self.indice_frame) % len(self.frames)]
 
     def update(self, dt):
         self.direção = input_jogador()
