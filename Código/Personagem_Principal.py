f4facefc0264a6180b162eca49cbd31064998df8
MrValeriano <jpsvalerius@gmail.com>
2025-07-18 13:34:37 +0100

    Update Personagem_Principal.py

--- Código/Personagem_Principal.py
+++ Código/Personagem_Principal.py
@@ -4,6 +4,10 @@ from Processamento_do_input_do_jogador import input_jogador
 class Principal(pygame.sprite.Sprite):
     def __init__(self, pos, *groups):
         super().__init__(*groups)
+        self.frames = {
+            "parado": importar_pasta("..","Grafismos","Personagem_Principal","Idle"),
+            
+        }
         self.image = pygame.image.load("Grafismos/Personagem_Principal/Idle/pixil-frame-0.png")
         self.rect = self.image.get_frect(center = pos)
         self.direção = vector()
