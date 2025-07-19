a5dbac177ce7fbdc7aa72118f814a5a4a27e8d4f
MrValeriano <jpsvalerius@gmail.com>
2025-07-19 16:57:29 +0100

    Update Itens.py

--- Código/Itens.py
+++ Código/Itens.py
@@ -1,12 +1,14 @@
 from Definições import *
 
 class Itens(pygame.sprite.Sprite):
-    def __init__(self, pos, *groups):
+    def __init__(self, tipo, âncora, *groups):
         super().__init__(*groups)
-        self.frames = importar_pasta("Grafismos","Personagem_Principal","Idle","Direita")
+        self.âncora = âncora
+        self.frames = importar_pasta("Grafismos", "Itens", tipo)
         self.indice_frame = 0
+        self.pos = (self.âncora.rect.midtop[0], self.âncora.rect.midtop[1] - (self.image.height / 2))
         self.image = self.frames[self.indice_frame]
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(center = self.pos)
         self.direção = vector()
     
     def animação(self, dt):
