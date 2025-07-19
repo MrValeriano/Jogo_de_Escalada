ff7a65aff20e04a6721186c61df30bcb65dc43f4
MrValeriano <jpsvalerius@gmail.com>
2025-07-19 17:34:01 +0100

    Update Itens.py

--- Código/Itens.py
+++ Código/Itens.py
@@ -6,16 +6,26 @@ class Itens(pygame.sprite.Sprite):
         self.âncora = âncora
         self.frames = importar_pasta("Grafismos", "Itens", tipo)
         self.indice_frame = 0
-        self.pos = (self.âncora.rect.midtop[0], self.âncora.rect.midtop[1] - (self.image.height / 2))
         self.image = self.frames[self.indice_frame]
+        self.pos = (self.âncora.rect.midtop[0], self.âncora.rect.midtop[1] - self.image.height)
         self.rect = self.image.get_frect(center = self.pos)
-        self.direção = vector()
+        self.direcção = vector()
+        self.acção = "não brilhar"
+        self.freq = ["brilhar"]*3 + ["não brilhar"]*7
     
+    def flutuar(self, dt):
+        pass
+            
+            
     def animação(self, dt):
         self.indice_frame += ANIMATION_SPEED * dt
         if int(self.indice_frame) >= len(self.frames):
             self.indice_frame = 0
-        self.image = self.frames[int(self.indice_frame)]
+            self.acção = rd.sample(self.freq,1)[0]
+        if self.acção == "brilhar":
+            self.image = self.frames[int(self.indice_frame)]
+        else:
+            self.image = self.frames[0]
 
     def update(self, dt):
         self.animação(dt)
