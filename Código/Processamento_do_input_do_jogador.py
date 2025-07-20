3407e05e98adc148f93cc601faa2dd0ccddfeb5e
MrValeriano <jpsvalerius@gmail.com>
2025-07-20 11:08:02 +0100

    Update Processamento_do_input_do_jogador.py

--- Código/Processamento_do_input_do_jogador.py
+++ Código/Processamento_do_input_do_jogador.py
@@ -3,6 +3,7 @@ from Definições import *
 def input_jogador():
         keys = pygame.key.get_pressed()
         input_vector = vector()
+        other_action = "None"
         if keys[pygame.K_a]:
             input_vector.x -= 1
         if keys[pygame.K_d]:
@@ -11,4 +12,6 @@ def input_jogador():
             input_vector.y -= 1
         if keys[pygame.K_s]:
             input_vector.y += 1
-        return input_vector
+        if keys[pygame.K_SPACE]:
+            other_action = "collect"
+        return [input_vector, other_action]
