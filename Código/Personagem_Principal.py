7990de52daa8d273da7ba7919028e0e1684080a7
MrValeriano <jpsvalerius@gmail.com>
2025-07-20 12:49:07 +0100

    Update Personagem_Principal.py

--- Código/Personagem_Principal.py
+++ Código/Personagem_Principal.py
@@ -22,9 +22,12 @@ class Principal(pygame.sprite.Sprite):
                 "esquerda": importar_pasta("Grafismos","Personagem_Principal","Hang_Cycle","Esquerda")
             }
         }
+        self.dano = False
         self.acção = "parado"
         self.lado = "direita"
         self.indice_frame = 0
+        self.frames_invencibilidade = 0
+        self.tempo_invencibilidade = 0
         self.image = self.frames[self.acção][self.lado][self.indice_frame]
         self.mapa = mapa
         self.rect = self.image.get_frect(center = self.mapa.posição)
@@ -43,6 +46,19 @@ class Principal(pygame.sprite.Sprite):
         if int(self.indice_frame) >= len(self.frames[self.acção][self.lado]):
             self.indice_frame = 0
         self.image = self.frames[self.acção][self.lado][int(self.indice_frame)]
+        if self.dano == True:
+            self.frames_invencibilidade += ANIMATION_SPEED * dt
+            self.tempo_invencibilidade += dt
+            print(self.tempo_invencibilidade)
+            if int(self.frames_invencibilidade) % 4 == 0:
+                self.image.set_alpha(0)
+            else:
+                self.image.set_alpha(255)
+            if int(self.tempo_invencibilidade) >= 3:
+                self.dano = False
+        else:
+            self.tempo_invencibilidade = 0
+            self.frames_invencibilidade = 0
     
     def interação(self):
         print(self.rect.collidelist(self.mapa.lista_objectos["Moeda"]))
@@ -53,6 +69,12 @@ class Principal(pygame.sprite.Sprite):
             if self.mapa.lista_objectos["Moeda"][moeda].alive():
                 self.mapa.lista_objectos["Moeda"][moeda].kill()
                 self.inventário["Moedas"] += 1
+        if self.rect.collidelist(self.mapa.lista_objectos["Vespa"]) > -1:
+            if self.dano == False:
+                self.dano = True
+        if self.rect.collidelist(self.mapa.lista_objectos["Tartaruga"]) > -1:
+            if self.dano == False:
+                self.dano = True
 
     def update(self, dt):
         actividade = input_jogador()
