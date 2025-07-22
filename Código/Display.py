c2c015dfbcf5a12529bd6ab0d0d6f1831fa6c13a
MrValeriano <jpsvalerius@gmail.com>
2025-07-22 14:49:47 +0100

    Update Display.py

--- Código/Display.py
+++ Código/Display.py
@@ -1,5 +1,5 @@
 from Definições import *
 
 imagem_moeda = pygame.image.load(join("Grafismos", "Itens", "Moeda", "0.png")).convert_alpha()
-ui_moeda = pygame.transform.smoothscale(imagem_moeda, (imagem_moeda.width/2, imagem_moeda.height/2))
+ui_moeda = pygame.transform.smoothscale(imagem_moeda, (2*imagem_moeda.width/3, 2*imagem_moeda.height/3))
 rect_moeda = ui_moeda.get_frect(bottomright = (SCREEN_WIDTH, SCREEN_HEIGHT))
