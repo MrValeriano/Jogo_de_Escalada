24e55c709bdcdce1fcc86cf0ccd0f44ba6c85dc2
MrValeriano <jpsvalerius@gmail.com>
2025-07-25 12:55:07 +0100

    Create portas.tsx

---
+++ Grafismos/Mapa/Dados/portas.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="portas" tilewidth="104" tileheight="131" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="porta">
+  <image source="../Porta_aberta.png" width="104" height="131"/>
+ </tile>
+ <tile id="1" type="porta">
+  <image source="../Porta_fechada.png" width="104" height="131"/>
+ </tile>
+</tileset>
