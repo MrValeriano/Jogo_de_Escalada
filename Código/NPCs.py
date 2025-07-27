1394e3930e710e953cb938c6791cec7a209a2562
MrValeriano <jpsvalerius@gmail.com>
2025-07-27 12:53:44 +0100

    Update NPCs.py

--- Código/NPCs.py
+++ Código/NPCs.py
@@ -20,7 +20,12 @@ class NPC(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(center = pos)
     
     def animação(self, dt):
-        self.indice_frame += ANIMATION_SPEED * dt
+        jogador = self.groups()[0].sprites()[-1]
+        if jogador.rect.centerx > self.rect.centerx:
+            self.lado = "direita"
+        else:
+            self.lado = "esquerda"
+        self.indice_frame += (ANIMATION_SPEED / 2) * dt
         if int(self.indice_frame) >= len(self.frames[self.acção][self.lado]):
             self.indice_frame = 0
         self.image = self.frames[self.acção][self.lado][int(self.indice_frame)]
